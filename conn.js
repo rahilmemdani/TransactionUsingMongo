8263179886733a92530f66b9a473edc70449febc
Rahil Memdani <rmemdanib@gmail.com>
2021-05-23 00:14:28 +0530

    Changed for dockerfile

--- conn.js
+++ conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/transaction", { useNewUrlParser: true,useUnifiedTopology: true  } ,function(err){
+mongoose.connect("mongodb://mongo:27017/transaction", { useNewUrlParser: true,useUnifiedTopology: true  } ,function(err){
     if(!err){
         console.log('Success');
     }else{
